9f279bebcfbab867b243b4b7e10b51a5b4ff0960
Tasnim813 <tasnim3j@gmail.com>
2026-04-12 11:21:26 +0600

    first commit

---
+++ data-type.js
@@ -0,0 +1,19 @@
+// js is dynamic typed programming language
+let data=4;
+data='tormuj';
+data=false;
+data= {name:"bangi", marks:52}
+data=[455]
+data=undefined
+data=null
+console.log(typeof data)
+
+// js : primitive data type
+const price=50;
+const name='tormuj';
+const isPoor=false;
+let isKnown;
+
+// js Non-primitive data-type
+const student={name:'tbsas',marks:45,isMarried:false}
+const friend=['Tasnim','tawhida']
