d995d24b8a56de00f84dee1047b3868109ef297f
Tasnim813 <tasnim3j@gmail.com>
2026-04-14 10:22:26 +0600

    double-triple-equal

---
+++ double-equal.js
@@ -0,0 +1,14 @@
+// type convert
+
+// console.log(2==2);
+// console.log(2=='2');
+// console.log(1==1);
+console.log(true== 1);
+console.log(false==0);
+console.log(null==undefined);
+// console.log(0==='');
+// console.log([]==='');
+console.log([5]==5)
+console.log([5]=='5')
+console.log({}=={});
+console.log([]==[]);
